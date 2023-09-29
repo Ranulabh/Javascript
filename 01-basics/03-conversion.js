345434f2fe6ecf4f469b289db4afacff27d7b363
Ranulabh <133311622+Ranulabh@users.noreply.github.com>
2023-09-29 09:59:22 +0000

    Conversion of different datatypes

---
+++ 01-basics/03-conversion.js
@@ -0,0 +1,35 @@
+let score = "Ranu"
+
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "100" => 100
+// "13abs" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "Ranu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let numberBe = 44
+
+let stringNumber = String(numberBe)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************************Operations**************************
+
+
