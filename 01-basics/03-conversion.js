f1a2b2ab68ed25d5fd74e217f20f2a61cd035d5c
Ranulabh <133311622+Ranulabh@users.noreply.github.com>
2023-10-01 08:46:31 +0000

    Conversion Operations of js

--- 01-basics/03-conversion.js
+++ 01-basics/03-conversion.js
@@ -32,4 +32,44 @@ let stringNumber = String(numberBe)
 
 //***************************Operations**************************
 
+let value = 3
+let negative = -value
+
+// console.log(negative);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/4);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Ranu"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1 + "2" + "3");
+// console.log(1 + 2 + "3");
+// console.log("1" + 2 + 3);
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
 
