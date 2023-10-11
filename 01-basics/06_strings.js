2d27aeab46b8549774de4dd4570664dd0b38f547
Ranulabh <133311622+Ranulabh@users.noreply.github.com>
2023-10-11 07:07:38 +0000

    String and method in string

---
+++ 01-basics/06_strings.js
@@ -0,0 +1,41 @@
+const name = "Ranu "
+const repoCount = 1
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} my repo count is ${repoCount}`);
+
+
+
+const getName = new String("Ranu-lb-com")
+
+// console.log(getName[0]);
+// console.log(getName.__proto__);
+
+// console.log(getName.length);
+// console.log(getName.toUpperCase());
+
+console.log(getName.charAt(2));
+console.log(getName.indexOf('u'));
+
+const newString = getName.substring(1,4)
+// console.log(newString);
+
+const anotherString = getName.substring(-8,4)
+// console.log(anotherString);
+
+const newStringOne = "    Ranu    " 
+
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://potential-space-parakeet"
+
+console.log(url.replace('-', '@'));
+
+console.log(url.includes('space'));
+
+console.log(getName.split('-'));
+
+
+
